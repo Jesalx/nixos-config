fe91e016b48c7cfe54382cd9337caeefab193e0d
Jesal Patel <jesalx@users.noreply.github.com>
2024-08-06 11:07:48 -0400

    add update alias

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -7,8 +7,8 @@
         autosuggestion.enable = true;
         enableCompletion = true;
         shellAliases = {
-        nix-rebuild = "sudo nixos-rebuild switch --flake /home/jesal/nixos-config#default";
-        ".." = "cd ..";
+          nix-rebuild = "sudo nixos-rebuild switch --flake /home/jesal/nixos-config#default";
+          nix-update = "sudo nix flake update /home/jesal/nixos-config";
         };
     };
     
