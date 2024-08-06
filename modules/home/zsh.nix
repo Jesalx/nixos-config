3af1675bbd73d7ef26267fac95c5b8ec76c46e0a
Jesal Patel <jesalx@users.noreply.github.com>
2024-08-06 11:10:45 -0400

    additional aliases

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -7,8 +7,10 @@
         autosuggestion.enable = true;
         enableCompletion = true;
         shellAliases = {
+          nix-test = "sudo nixos-rebuild test --flake /home/jesal/nixos-config#default";
           nix-rebuild = "sudo nixos-rebuild switch --flake /home/jesal/nixos-config#default";
           nix-update = "sudo nix flake update /home/jesal/nixos-config";
+          nix-config = "nvim /home/jesal/nixos-config/";
         };
     };
     
