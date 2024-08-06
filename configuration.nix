0a4ae44cf9ce6888dfb01393583f589457c1faeb
Jesal Patel <jesalx@users.noreply.github.com>
2024-08-05 22:08:43 -0400

    move hyprland sys config into module

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./hyprland.nix
+      ./modules/nixos/hyprland.nix
       inputs.home-manager.nixosModules.default
     ];
 
