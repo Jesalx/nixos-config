b23c7600d545c897575032e314c43d20a1965f74
Jesal Patel <jesalx@users.noreply.github.com>
2024-08-07 20:57:44 -0400

    enable scheduled nix store optimizations

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -52,6 +52,11 @@
         options = "--delete-older-than 30d";
       };
 
+      optimise = {
+        automatic = true;
+        dates = [ "03:45" ];
+      };
+
       # Opinionated: make flake registry and nix path match flake inputs
       registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
       nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
