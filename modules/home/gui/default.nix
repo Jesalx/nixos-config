a54457bacb6f0c0ca648c5a6aeb24d962a56a188
Jesal Patel <jesalx@users.noreply.github.com>
2024-08-08 11:25:39 -0400

    move hyprlock to nix

--- modules/home/gui/default.nix
+++ modules/home/gui/default.nix
@@ -3,12 +3,14 @@
   imports = [
     ./hypr/hyprland.nix
     ./hypr/hyprpaper.nix
+    ./hypr/hyprlock.nix
     ./theme.nix
     ./wofi.nix
   ];
 
   hyprland.enable = false;
   hyprpaper.enable = false;
+  hyprlock.enable = false;
   theme.enable = true;
   wofi.enable = true;
 }
