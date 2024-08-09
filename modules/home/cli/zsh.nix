c48989689151354583e6833ac80d6d341d36aea6
Jesal Patel <jesalx@users.noreply.github.com>
2024-08-09 09:47:20 -0400

    add rebuild script

--- modules/home/cli/zsh.nix
+++ modules/home/cli/zsh.nix
@@ -6,6 +6,42 @@
 }:
 let
   user = config.home.username;
+  nix-rebuild-app = pkgs.writeShellApplication {
+    name = "nix-rebuild";
+    runtimeInputs = with pkgs; [ git ];
+    text = # bash
+      ''
+        set -euo pipefail
+
+        perform_rebuild() {
+          local action=$1
+          
+          pushd . > /dev/null
+
+          cd /home/${user}/nixos-config
+
+          git add --intent-to-add .
+
+          if [ "$action" = "test" ]; then
+            sudo nixos-rebuild test --flake .#default
+          elif [ "$action" = "switch" ]; then
+            sudo nixos-rebuild switch --flake .#default
+          else
+            echo "Invalid action. Use 'test' or 'switch'."
+            exit 1
+          fi
+
+          popd > /dev/null
+        }
+
+        if [ $# -eq 0 ]; then
+          echo "Usage: nix-rebuild-app [test|switch]"
+          exit 1
+        fi
+
+        perform_rebuild "$1"
+      '';
+  };
 in
 {
   options = {
@@ -15,8 +51,8 @@ in
     programs.zsh = {
       enable = true;
       shellAliases = {
-        nix-test = "sudo nixos-rebuild test --flake /home/${user}/nixos-config#default";
-        nix-rebuild = "sudo nixos-rebuild switch --flake /home/${user}/nixos-config#default";
+        nix-test = "${nix-rebuild-app}/bin/nix-rebuild test";
+        nix-rebuild = "${nix-rebuild-app}/bin/nix-rebuild switch";
         nix-update = "sudo nix flake update /home/${user}/nixos-config";
         nix-config = "nvim /home/${user}/nixos-config/";
         nix-gc = "sudo nix-collect-garbage --delete-older-than 30d";
