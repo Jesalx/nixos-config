963857eed2ca4bcc57972f150f4d8cd473d2c522
Jesal Patel <jesalx@users.noreply.github.com>
2024-08-13 13:27:12 -0400

    update nvim config
    update nvim config

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -11,17 +11,17 @@ return {
       },
     },
   },
-  {
-    "srcery-colors/srcery-vim",
-    lazy = true,
-    config = function()
-      -- set this to black so that popup windows are black/transparent
-      vim.g.srcery_xgray2 = "#000000"
-      vim.g.srcery_xgray2_cterm = 0
-      vim.g.srcery_bg = { "#000000", "NONE" }
-      vim.g.srcery_inverse = 0
-    end,
-  },
+  -- {
+  --   "srcery-colors/srcery-vim",
+  --   lazy = true,
+  --   config = function()
+  --     -- set this to black so that popup windows are black/transparent
+  --     vim.g.srcery_xgray2 = "#000000"
+  --     vim.g.srcery_xgray2_cterm = 0
+  --     vim.g.srcery_bg = { "#000000", "NONE" }
+  --     vim.g.srcery_inverse = 0
+  --   end,
+  -- },
   {
     "catppuccin/nvim",
     name = "catppuccin",
