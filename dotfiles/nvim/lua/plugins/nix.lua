2338b571318506e3c91dce14cc5f209eaa6ec5be
Jesal Patel <jesalx@users.noreply.github.com>
2024-08-13 17:20:17 -0400

    update nix language server stuff
    update nix lsp
    
    add vim sleuth

--- dotfiles/nvim/lua/plugins/nix.lua
+++ dotfiles/nvim/lua/plugins/nix.lua
@@ -1,21 +1,17 @@
 -- File: ~/.config/nvim/lua/plugins/nix.lua
-
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        nil_ls = {
-          settings = {
-            ["nil"] = {
-              formatting = {
-                command = { "nixfmt" },
-              },
-            },
-          },
-        },
-      },
-    },
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.nil_ls = {
+        mason = false, -- Disable Mason for this server
+        cmd = { "nil" },
+        filetypes = { "nix" },
+        root_dir = require("lspconfig.util").root_pattern("flake.nix", ".git"),
+      }
+      return opts
+    end,
   },
   {
     "stevearc/conform.nvim",
