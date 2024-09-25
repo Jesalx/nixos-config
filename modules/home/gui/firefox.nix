a6656739790535841b62f0d7480f5779c09ffd0e
Jesal Patel <jesalx@users.noreply.github.com>
2024-09-25 10:58:03 -0400

    remove search stuff

--- modules/home/gui/firefox.nix
+++ modules/home/gui/firefox.nix
@@ -29,40 +29,40 @@
           "signon.generation.enabled" = false;
           "browser.uidensity" = 1;
         };
-        search = {
-          # force = true;
-          default = "Kagi";
-          engines = {
-            "Nix Packages" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "channel";
-                      value = "unstable";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@nix" ];
-            };
-
-            "Bing".metaData.hidden = true;
-            "eBay".metaData.hidden = true;
-          };
-          order = [
-            "Kagi"
-            "Google"
-            "DuckDuckGo"
-            "Nix Packages"
-          ];
-        };
+        # search = {
+        #   # force = true;
+        #   default = "Kagi";
+        #   engines = {
+        #     "Nix Packages" = {
+        #       urls = [
+        #         {
+        #           template = "https://search.nixos.org/packages";
+        #           params = [
+        #             {
+        #               name = "channel";
+        #               value = "unstable";
+        #             }
+        #             {
+        #               name = "query";
+        #               value = "{searchTerms}";
+        #             }
+        #           ];
+        #         }
+        #       ];
+        #       icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+        #       definedAliases = [ "@nix" ];
+        #     };
+        #
+        #     "Bing".metaData.hidden = true;
+        #     "eBay".metaData.hidden = true;
+        #   };
+        #   order = [
+        #     "Kagi"
+        #     "Google"
+        #     "DuckDuckGo"
+        #     "Nix Packages"
+        #   ];
+        # };
       };
       policies = {
         DisableTelemetry = true;
