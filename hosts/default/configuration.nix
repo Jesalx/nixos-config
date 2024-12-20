27dd0454e174149b4e700d49b12d501f88a130d8
Jesal Patel <mail@jesal.dev>
2024-12-20 13:41:34 -0500

    add stuff for keyboard config

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -176,6 +176,7 @@
 
   # Allow access to keyboards
   hardware.keyboard.qmk.enable = true;
+  hardware.keyboard.zsa.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
--- modules/home/cli/development.nix
+++ modules/home/cli/development.nix
@@ -36,6 +36,7 @@
       zls
       qmk
       via
+      chromium
     ];
   };
 }
