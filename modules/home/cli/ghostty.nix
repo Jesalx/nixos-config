1abcf156110f36cc2d4ba74d26ef276022568707
Jesal Patel <mail@jesal.dev>
2025-05-26 15:33:00 -0400

    update ghostty theme

--- modules/home/cli/ghostty.nix
+++ modules/home/cli/ghostty.nix
@@ -10,13 +10,33 @@
   };
   config = lib.mkIf config.ghostty.enable {
     xdg.configFile."ghostty/config".text = ''
-      theme = catppuccin-mocha
       background = 000000
       background-opacity = 0.8
       background-blur-radius = 20
       window-decoration = false
       gtk-titlebar = false
       font-family = Berkeley Mono Variable
+      # https://github.com/srcery-colors/srcery-terminal/
+      foreground = #fce8c3
+      selection-foreground = #1c1b19
+      selection-background = #fce8c3
+
+      palette = 0=#1c1b19
+      palette = 1=#ef2f27
+      palette = 2=#519f50
+      palette = 3=#fbb829
+      palette = 4=#2c78bf
+      palette = 5=#e02c6d
+      palette = 6=#0aaeb3
+      palette = 7=#baa67f
+      palette = 8=#918175
+      palette = 9=#f75341
+      palette = 10=#98bc37
+      palette = 11=#fed06e
+      palette = 12=#68a8e4
+      palette = 13=#ff5c8f
+      palette = 14=#2be4d0
+      palette = 15=#fce8c3
     '';
   };
 }
