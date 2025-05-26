a4fea4eddcec218ceb57109231935629b34045c9
Jesal Patel <mail@jesal.dev>
2025-05-26 12:59:43 -0400

    add copilot

---
+++ dotfiles/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-.>", 'copilot#Accept("<CR>")', { silent = true, expr = true, desc = "Accept Copilot suggestion" })
+
+      vim.api.nvim_create_user_command("CopilotToggle", function()
+        local copilot_status = vim.g.copilot_enabled
+        if copilot_status == nil or copilot_status == 1 then
+          vim.cmd("Copilot disable")
+          vim.g.copilot_enabled = 0
+          print("Copilot disabled")
+        else
+          vim.cmd("Copilot enable")
+          vim.g.copilot_enabled = 1
+          print("Copilot enabled")
+        end
+      end, { desc = "Toggle Copilot on/off" })
+
+      vim.keymap.set("n", "<leader>cc", "<cmd>CopilotToggle<cr>", { desc = "[C]opilot toggle" })
+    end,
+  },
+}
