9e11b2faae7e479ed2b2898a5e9e57e33fbfe61e
Jesal Patel <mail@jesal.dev>
2025-05-26 16:01:51 -0400

    add yanking shortcut

--- dotfiles/nvim/lua/keymaps.lua
+++ dotfiles/nvim/lua/keymaps.lua
@@ -26,6 +26,8 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<leader>y", ":%y<CR>", { desc = "[Y]ank entire file" })
+
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
