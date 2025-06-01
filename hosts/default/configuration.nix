db712e91bfcb5a846624277515f839b2864e2c33
Jesal Patel <mail@jesal.dev>
2025-06-01 13:06:22 -0400

    fix warning for xserver param renaming

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -126,8 +126,7 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
