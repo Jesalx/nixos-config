3303716d0b7a9ab532d6e9d4142bdaff17bfd510
Jesal Patel <mail@jesal.dev>
2025-10-24 19:47:09 -0400

    fix(helium): use app image

--- packages/helium/default.nix
+++ packages/helium/default.nix
@@ -1,81 +1,44 @@
 {
-lib,
-unzip,
-autoPatchelfHook,
-stdenv,
-fetchurl,
-xorg,
-libgbm,
-cairo,
-libudev-zero,
-libxkbcommon,
-nspr,
-nss,
-libsForQt5,
-libcupsfilters,
-qt6,
-pango
- }:
+  stdenv,
+  lib,
+  appimageTools,
+  fetchurl,
+  makeDesktopItem,
+  copyDesktopItems,
+}:
+let
+  pname = "helium-browser";
+  version = "0.5.8.1";
 
-stdenv.mkDerivation rec {
-    name = "Helium";
-    version = "0.4.7.1";
-
-    src = fetchurl {
-	url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64_linux.tar.xz";
-        sha256 = "sha256:1a475cacdfdc900dbaa905a476f5ae82e9d62a30af735b92c456dbc3171b44b2";
+  architectures = {
+    "x86_64-linux" = {
+      arch = "x86_64";
+      hash = "sha256-d8kwLEU6qgEgp7nlEwdfRevB1JrbEKHRe8+GhGpGUig=";
     };
-
-    nativeBuildInputs = [ 
-        unzip
-        autoPatchelfHook
-    ];
-    
-    buildInputs = [
-        unzip
-        xorg.libxcb
-        xorg.libX11
-        xorg.libXcomposite
-        xorg.libXdamage
-        xorg.libXext
-        xorg.libXfixes
-        xorg.libXrandr
-        libgbm
-        cairo
-        pango
-        libudev-zero
-        libxkbcommon
-        nspr
-        nss
-        libsForQt5.qtbase
-        qt6.full
-        libcupsfilters
-        qt6.wrapQtAppsHook
-    ];
-
-    installPhase = ''
-        runHook preInstall
-        mkdir -p $out/bin
-        mv * $out/bin/
-        mv $out/bin/chrome $out/bin/${name}
-        mkdir -p $out/share/applications
-        
-        cat <<INI> $out/share/applications/${name}.desktop
-[Desktop Entry]
-Name=${name}
-GenericName=Web Browser
-Terminal=false
-Icon=$out/bin/product_logo_256.png
-Exec=$out/bin/${name}
-Type=Application
-Categories=Network;WebBrowser;
-INI
-        '';
-
-
-    meta = with lib; {
-        homepage = "https://github.com/imputnet/helium-linux";
-        description = "A description of your application";
-        platforms = platforms.linux;
+    "aarch64-linux" = {
+      arch = "arm64";
+      hash = "sha256-KfQlOT4mMKQ40B8hWl+GlmRNVhZnEln59ptfXN0XCLc=";
     };
+  };
+
+  src =
+    let
+      inherit (architectures.${stdenv.hostPlatform.system}) arch hash;
+    in
+    fetchurl {
+      url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-${arch}.AppImage";
+      inherit hash;
+    };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+  nativeBuildInputs = [ copyDesktopItems ];
+  desktopItems = [
+    (makeDesktopItem {
+
+    })
+  ];
+  meta = {
+    platforms = lib.attrNames architectures;
+  };
 }
