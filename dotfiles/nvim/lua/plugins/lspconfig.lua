4220240e7f2a7e6d25be0125716acf3811188bec
Jesal Patel <mail@jesal.dev>
2026-02-04 19:32:42 -0500

    clean up

--- dotfiles/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/plugins/lspconfig.lua
@@ -44,115 +44,63 @@ return {
         },
       })
 
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-      --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
         callback = function(event)
-          -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-          -- to define small helper and utility functions so you don't have to repeat yourself.
-          --
-          -- In this case, we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
-          local map = function(keys, func, desc, mode)
-            mode = mode or "n"
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
-
           for _, k in ipairs({ "gra", "grn", "grr", "gri" }) do
             pcall(vim.keymap.del, "n", k)
           end
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = event.buf, desc = "[R]e[n]ame" })
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+          vim.keymap.set({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf, desc = "[C]ode [A]ction" })
 
           -- Find references for the word under your cursor.
-          map("gr", function()
+          vim.keymap.set("n", "gr", function()
             Snacks.picker.lsp_references()
-          end, "[G]oto [R]eferences", "n")
+          end, { buffer = event.buf, desc = "[G]oto [R]eferences" })
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map("gi", function()
+          vim.keymap.set("n", "gi", function()
             Snacks.picker.lsp_implementations()
-          end, "[G]oto [I]mplementation")
+          end, { buffer = event.buf, desc = "[G]oto [I]mplementation" })
 
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map("gd", function()
+          vim.keymap.set("n", "gd", function()
             Snacks.picker.lsp_definitions()
-          end, "[G]oto [D]efinition")
+          end, { buffer = event.buf, desc = "[G]oto [D]efinition" })
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
-          map("gD", function()
+          vim.keymap.set("n", "gD", function()
             Snacks.picker.lsp_declarations()
-          end, "[G]oto [D]eclaration")
+          end, { buffer = event.buf, desc = "[G]oto [D]eclaration" })
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map("gO", function()
+          vim.keymap.set("n", "gO", function()
             Snacks.picker.lsp_symbols()
-          end, "Open [D]ocument Symbols")
+          end, { buffer = event.buf, desc = "Open [D]ocument Symbols" })
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map("gW", function()
+          vim.keymap.set("n", "gW", function()
             Snacks.picker.lsp_workspace_symbols()
-          end, "Open [W]orkspace Symbols")
+          end, { buffer = event.buf, desc = "Open [W]orkspace Symbols" })
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map("gt", function()
+          vim.keymap.set("n", "gt", function()
             Snacks.picker.lsp_type_definitions()
-          end, "[G]oto [T]ype Definition")
-
-          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has("nvim-0.11") == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
+          end, { buffer = event.buf, desc = "[G]oto [T]ype Definition" })
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
@@ -160,7 +108,7 @@ return {
           --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
             local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
             vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
               buffer = event.buf,
@@ -184,11 +132,11 @@ return {
           end
 
           -- The following code creates a keymap to toggle inlay hints in the code
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
             vim.lsp.inlay_hint.enable(true, { bufnr = event.buf }) -- enable inlay hints by default
-            map("<leader>th", function()
+            vim.keymap.set("n", "<leader>th", function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-            end, "[T]oggle Inlay [H]ints")
+            end, { buffer = event.buf, desc = "[T]oggle Inlay [H]ints" })
           end
         end,
       })
