71ee7b368c9597008fe32df11a940c052e8b16d8
Jesal Patel <jesal.patel@weather.com>
2026-02-05 09:30:20 -0500

    update neovim keybindings

--- dotfiles/nvim/lua/keymaps.lua
+++ dotfiles/nvim/lua/keymaps.lua
@@ -28,3 +28,9 @@ vim.keymap.set("n", "<leader>y", ":%y<CR>", { desc = "[Y]ank buffer" })
 
 -- Toggle autoformat on save
 vim.keymap.set("n", "<leader>tf", "<cmd>ToggleFormat<CR>", { desc = "[T]oggle [F]ormat on save" })
+
+-- Restart Neovim
+vim.keymap.set('n', '<leader>R', '<cmd>restart<cr>', { desc = '[R]estart Neovim' })
+
+-- Open package manager
+vim.keymap.set('n', '<leader>L', '<cmd>Lazy<cr>', { desc = 'Lazy' })
