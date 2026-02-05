6d3351a756190183bbcabb99c776cbe11815e63b
Jesal Patel <jesal.patel@weather.com>
2026-02-05 09:26:07 -0500

    remove mini.surround from neovim

--- dotfiles/nvim/lua/plugins/mini.lua
+++ dotfiles/nvim/lua/plugins/mini.lua
@@ -1,28 +1,8 @@
 return {
-  { -- Collection of various small independent plugins/modules
+  {
     "echasnovski/mini.nvim",
-    keys = {
-      { "sa", mode = { "n", "v" } },
-      { "sd", mode = { "n", "v" } },
-      { "sr", mode = { "n", "v" } },
-      { "a", mode = { "x", "o" } },
-      { "i", mode = { "x", "o" } },
-    },
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require("mini.ai").setup({ n_lines = 500 })
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require("mini.surround").setup()
+      require("mini.ai").setup({ n_lines = 50 })
     end,
   },
 }
