abde9fe20a0812728e49247b21911ed06667420b
Jesal Patel <mail@jesal.dev>
2026-03-30 17:57:50 -0400

    refactor(nvim): move PackChanged autocmd to autocmds.lua

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -7,23 +7,3 @@ _G._init_start = vim.uv.hrtime()
 require('options')
 require('autocmds')
 require('keymaps')
-
--- Plugin hooks must be defined before the first vim.pack.add() call
--- (plugin/ files are sourced after init.lua, so this ordering is guaranteed)
-vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if name == 'nvim-treesitter' and (kind == 'install' or kind == 'update') then
-      if not ev.data.active then
-        vim.cmd.packadd('nvim-treesitter')
-      end
-      vim.cmd('TSUpdate')
-    end
-
-    -- Build blink.cmp's Rust fuzzy matcher from source after install/update
-    if name == 'blink.cmp' and (kind == 'install' or kind == 'update') then
-      vim.system({ 'cargo', 'build', '--release' }, { cwd = ev.data.path }):wait()
-    end
-  end,
-  desc = 'Post-install/update hooks for plugins',
-})
