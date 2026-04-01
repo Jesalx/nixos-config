8d4ea659b3cc5e99a1f8325d1c691517ae1abdf7
Jesal Patel <mail@jesal.dev>
2026-04-01 18:53:37 -0400

    refactor(nvim): move mini.pick keymaps from snacks file to mini file

--- dotfiles/nvim/plugin/00-mini.lua
+++ dotfiles/nvim/plugin/00-mini.lua
@@ -7,6 +7,36 @@ require('mini.pairs').setup()
 require('mini.pick').setup()
 require('mini.extra').setup()
 
+-- Picker keymaps
+
+vim.keymap.set('n', '<leader><leader>', function()
+  MiniPick.builtin.files()
+end, { desc = '[ ] Search Files' })
+
+vim.keymap.set('n', '<leader>/', function()
+  MiniPick.builtin.grep_live()
+end, { desc = '[/] Search by Grep' })
+
+vim.keymap.set('n', '<leader>sh', function()
+  MiniPick.builtin.help()
+end, { desc = '[S]earch [H]elp' })
+
+vim.keymap.set('n', '<leader>sk', function()
+  MiniExtra.pickers.keymaps()
+end, { desc = '[S]earch [K]eymaps' })
+
+vim.keymap.set('n', '<leader>sf', function()
+  MiniPick.builtin.files()
+end, { desc = '[S]earch [F]iles' })
+
+vim.keymap.set('n', '<leader>s.', function()
+  MiniExtra.pickers.oldfiles()
+end, { desc = '[S]earch Recent Files (["."] for repeat)' })
+
+vim.keymap.set('n', '<leader>sd', function()
+  MiniExtra.pickers.diagnostic()
+end, { desc = '[S]earch [D]iagnostics' })
+
 -- Snacks loads after mini alphabetically (00-mini < 00-snacks),
 -- so defer the toggle registration until all plugin/ files are sourced
 vim.schedule(function()
--- dotfiles/nvim/plugin/00-snacks.lua
+++ dotfiles/nvim/plugin/00-snacks.lua
@@ -204,34 +204,6 @@ require('snacks').setup({
 
 -- Keymaps
 
-vim.keymap.set('n', '<leader><leader>', function()
-  MiniPick.builtin.files()
-end, { desc = '[ ] Search Files' })
-
-vim.keymap.set('n', '<leader>/', function()
-  MiniPick.builtin.grep_live()
-end, { desc = '[/] Search by Grep' })
-
-vim.keymap.set('n', '<leader>sh', function()
-  MiniPick.builtin.help()
-end, { desc = '[S]earch [H]elp' })
-
-vim.keymap.set('n', '<leader>sk', function()
-  MiniExtra.pickers.keymaps()
-end, { desc = '[S]earch [K]eymaps' })
-
-vim.keymap.set('n', '<leader>sf', function()
-  MiniPick.builtin.files()
-end, { desc = '[S]earch [F]iles' })
-
-vim.keymap.set('n', '<leader>s.', function()
-  MiniExtra.pickers.oldfiles()
-end, { desc = '[S]earch Recent Files (["."] for repeat)' })
-
-vim.keymap.set('n', '<leader>sd', function()
-  MiniExtra.pickers.diagnostic()
-end, { desc = '[S]earch [D]iagnostics' })
-
 vim.keymap.set('n', '<leader>e', function()
   Snacks.explorer.open()
 end, { desc = '[E]xplorer' })
