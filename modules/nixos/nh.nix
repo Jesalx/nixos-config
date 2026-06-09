fc09c73da427414a04440d5707f1fd5d36d76d55
Jesal Patel <mail@jesal.dev>
2026-06-09 16:03:20 -0400

    chore(nixos): trim nh clean retention to 10 generations
    - lower programs.nh.clean.extraArgs --keep from 30 to 10
    - remove programs.steam.gamescopeSession.enable

--- modules/nixos/nh.nix
+++ modules/nixos/nh.nix
@@ -11,7 +11,7 @@
     programs.nh = {
       enable = true;
       clean.enable = true;
-      clean.extraArgs = "--keep-since 7d --keep 30";
+      clean.extraArgs = "--keep-since 7d --keep 10";
       flake = "/home/${userConfig.user}/nixos-config";
     };
   };
--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -14,7 +14,6 @@
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
-      gamescopeSession.enable = true;
     };
 
     programs.gamemode.enable = true;
